b8ab3f15838cf9e40cca6832b981564c491aca31
Brent Hronk <brenthronk@Brents-MacBook-Air.local>
2013-02-19 20:58:33 -0700

    initial commit

---
+++ Fitsby/LandingViewController.h
@@ -0,0 +1,13 @@
+//
+//  LandingViewController.h
+//  Fitsby
+//
+//  Created by Brent Hronk on 2/19/13.
+//  Copyright (c) 2013 Fitsby. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface LandingViewController : UIViewController
+
+@end
