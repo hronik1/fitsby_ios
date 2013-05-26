8d10e9aaaa26e4c8ed6a8c154550cab441d9fd19
Brent Hronik <brenthronk@Brents-MacBook-Air.local>
2013-05-26 11:37:37 -0600

    remove dead code in LandingViewController

--- Fitsby/LandingViewController.m
+++ Fitsby/LandingViewController.m
@@ -16,10 +16,7 @@
 
 - (IBAction)cancel:(UIStoryboardSegue *)segue
 {
-    //TODO possible implement
-//    if ([[segue identifier] isEqualToString:@"CancelInput"]) {
-//        [self dismissViewControllerAnimated:YES completion:NULL];
-//    }
+    
 }
 
 - (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
