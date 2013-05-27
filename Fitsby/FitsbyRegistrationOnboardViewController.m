28eef5c1d1843e02670dbc1c06b02e107711ad7e
Brent Hronik <brenthronk@Brents-MacBook-Air.local>
2013-05-27 10:47:46 -0600

    FitsbyRegistrationOnboardViewController make function comment space consistent

--- Fitsby/FitsbyRegistrationOnboardViewController.m
+++ Fitsby/FitsbyRegistrationOnboardViewController.m
@@ -21,7 +21,7 @@ static NSString *const SEGUE_ID = @"registrationOnboardComplete";
  *
  * Returns the index of the currently displayed view
  *
- * @return The index of the currently displayed view, or -1 on fail.
+ * @return  The index of the currently displayed view, or -1 on fail.
  */
 - (NSUInteger)indexOfCurrentViewController;
 
