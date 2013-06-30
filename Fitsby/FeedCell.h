1a648ad49e1114d5e48de3a0d2cfb86a30672a23
Anirudh Balagopal <anirudhbg@gmail.com>
2013-06-30 00:29:22 -0400

    NewsFeed in Progress
    NewsFeed in Progress

---
+++ Fitsby/FeedCell.h
@@ -0,0 +1,13 @@
+//
+//  FeedCell.h
+//  Fitsby
+//
+//  Created by Anirudh Balagopal on 29/06/13.
+//  Copyright (c) 2013 Fitsby. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface FeedCell : NSObject
+
+@end
---
+++ Fitsby/FeedCell.m
@@ -0,0 +1,13 @@
+//
+//  FeedCell.m
+//  Fitsby
+//
+//  Created by Anirudh Balagopal on 29/06/13.
+//  Copyright (c) 2013 Fitsby. All rights reserved.
+//
+
+#import "FeedCell.h"
+
+@implementation FeedCell
+
+@end
